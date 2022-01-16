57b417c0f8778331aaadd845b29b972293f929a1
Danilo Silva <danilo.silva@msn.com>
2022-01-16 20:40:52 -0300

    Implementando Rotas Inteligentes

---
+++ MinhaDemoMvc2/MinhaDemoMvc2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MinhaDemoMvc2.Models;
+using System;
+using System.Diagnostics;
+
+namespace MinhaDemoMvc2.Controllers
+{
+    [Route("")]
+    [Route("gestao-clientes")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        [Route("pagina-inicial")]
+        [Route("pagina-inicial/{id}")]
+        [Route("pagina-inicial/{id:int}/{categoria:guid}")]
+        public IActionResult Index(int id, Guid categoria)
+        {
+            return View();
+        }
+
+        [Route("privacidade")]
+        [Route("politica-privacidade")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro-encontrado")]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
