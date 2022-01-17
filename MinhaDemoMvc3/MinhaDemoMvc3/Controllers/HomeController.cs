1b7263bec0d8078c4131d46c8f1fa3bdbc9a871b
Danilo Silva <danilo.silva@msn.com>
2022-01-16 22:36:08 -0300

    Trabalhando com Action Results

---
+++ MinhaDemoMvc3/MinhaDemoMvc3/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MinhaDemoMvc3.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaDemoMvc3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            //return Json("{'Nome:' 'Danilo'}");
+
+            //var fileBytes = System.IO.File.ReadAllBytes(@"C:\Projetos\EduardoPires-Dominando-AspNetMvcCore\arquivo.txt");
+            //var fileName = "ola.txt";
+            //return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+
+            return Content("Hello World!");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
