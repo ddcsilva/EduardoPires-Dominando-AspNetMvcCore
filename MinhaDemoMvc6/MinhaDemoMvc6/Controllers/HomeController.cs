de675d0ab1776f06148d2341b55a265a32f8a25b
Danilo Silva <danilo.silva@msn.com>
2022-01-17 00:08:25 -0300

    Validação de Modelos

---
+++ MinhaDemoMvc6/MinhaDemoMvc6/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MinhaDemoMvc5.Models;
+using MinhaDemoMvc6.Models;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace MinhaDemoMvc6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var filme = new Filme
+            {
+                Titulo = "Oi",
+                DataLancamento = DateTime.Now,
+                Genero = null,
+                Avaliacao = 10,
+                Valor = 2000
+            };
+
+            return RedirectToAction("Privacy", filme);
+            //return View();
+        }
+
+        public IActionResult Privacy(Filme filme)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            foreach (var error in ModelState.Values.SelectMany(m => m.Errors))
+            {
+                Debug.WriteLine(error.ErrorMessage);
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
